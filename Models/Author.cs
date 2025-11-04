e24a573dc40468c18e789bfa6a950a1e475eea87
denisarambasic <denis.arambasic@gmail.com>
2025-11-04 20:11:40 +0100

    Created parcijalni ispit TodoApp

---
+++ Models/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNet.TODO.Models;
+
+public class Author
+{
+    public int AuthorId { get; set; }
+
+    public string Name { get; set; }
+
+    public string Bio { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using AspNet.TODO.Models;
 using AspNet.TODO.Repository;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace AspNet.TODO
 {
@@ -13,6 +16,33 @@ namespace AspNet.TODO
             builder.Services.AddScoped<ITodoRepository, TodoRepository>();
 
             var app = builder.Build();
+            
+            //var connectionString = "Data Source=DESKTOP-TLDLP4N;Initial Catalog=Algebra;Trusted_Connection=true;Encrypt=false;";
+            var connectionString = app.Configuration.GetConnectionString("DefaultConnection");
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            //using var command = new SqlCommand("INSERT INTO Author (Name, Bio) VALUES('Pero', '');", connection);
+            //command.ExecuteNonQuery();
+            //using var command = new SqlCommand("SELECT 1;", connection);
+            //using var command = new SqlCommand("SELECT [Name] FROM Author;", connection);
+            //using var command = new SqlCommand("SELECT * FROM Author WHERE AuthorId = @book_id", connection);
+            //command.Parameters.Clear();
+            //command.Parameters.AddWithValue("@book_id", 1);
+            using var command = new SqlCommand("SELECT * FROM Author", connection);
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var id = reader[0];
+                var authorId = reader["AuthorId"];
+                var authorName = reader["Name"];
+                var bio = reader["Bio"];
+            }
+            var result =  command.ExecuteScalar();
+
+
+            //Todo:...
+            connection.Close();
+            connection.Dispose();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
